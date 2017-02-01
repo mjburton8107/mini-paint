b90bfcd3712811e0044ac709051cbfb453827dd2
MikeB <burton.mj@gmail.com>
2017-02-01 14:50:59 -0700

    redid the exercise and coded myself; mostly

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
-var color = 'white';
-var colors = 'red blue green yellow white'
 
-$(document).ready(function() {
-  $('.box').on('click', function(){
-    $(this).addClass(color);
+$(document).ready(function(){
+  var color = "white";
+  var colors = "red green blue yellow white"
+
+  $(".box").on("click", function(){
+    $(this).toggleClass(color);
   });
-  $('.box').on('dblclick', function(){
-    $(this).removeClass(colors);
+  $(".box").on("dblclick", function(){
+    $(this).removeClass(color);
   })
-  $('#reset').on('click', function(){
-    $('.box').removeClass(colors);
+  $("#reset").on("click", function(){
+    $(".box").removeClass(colors);
   })
+  $("#red").on("click", function(){
+    color = "red";
+  })
+  $("#green").on("click", function(){
+    color = "green";
+  })
+  $("#blue").on("click", function(){
+    color = "blue";
+  })
+  $("#yellow").on("click", function(){
+    color = "yellow";
+  })
+  $("#white").on("click", function(){
+    color = "white";
   })
 
-
-$('#red').on('click', function(){
-  color = 'red';
-});
-
-$('#green').on('click', function(){
-  color = 'green';
-});
-
-$('#blue').on('click', function(){
-  color = 'blue';
-});
-
-$('#yellow').on('click', function(){
-  color = 'yellow';
-});
-
-$('#white').on('click', function(){
-  color = 'white';
 });
