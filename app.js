1e889c3555ec975cd0fa39391e0c0452c1d0e318
MikeB <burton.mj@gmail.com>
2017-01-31 18:52:34 -0700

    jquery finished - now toggle

---
+++ app.js
@@ -0,0 +1,35 @@
+var color = 'white';
+var colors = 'red blue green yellow white'
+
+$(document).ready(function() {
+  $('.box').on('click', function(){
+    $(this).addClass(color);
+  });
+  $('.box').on('dblclick', function(){
+    $(this).removeClass(colors);
+  })
+  $('#reset').on('click', function(){
+    $('.box').removeClass(colors);
+  })
+  })
+
+
+$('#red').on('click', function(){
+  color = 'red';
+});
+
+$('#green').on('click', function(){
+  color = 'green';
+});
+
+$('#blue').on('click', function(){
+  color = 'blue';
+});
+
+$('#yellow').on('click', function(){
+  color = 'yellow';
+});
+
+$('#white').on('click', function(){
+  color = 'white';
+});
